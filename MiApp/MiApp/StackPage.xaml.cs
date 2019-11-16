144a7ee4967bb2df2b8b8f9141ab100c5bec12bb
Javier Gonzalez <javier.glez.martin@gmail.com>
2019-11-16 12:32:28 +0100

    Sección 3, Video: 23: Crear / Modificar Stacklayouts desde el código en C#

--- MiApp/MiApp/StackPage.xaml.cs
+++ MiApp/MiApp/StackPage.xaml.cs
@@ -15,6 +15,25 @@ namespace MiApp
         public StackPage()
         {
             InitializeComponent();
+
+            var layout = new StackLayout
+            {
+                Spacing = 40,
+                Padding = new Thickness(0, 20, 0, 0),
+                Orientation = StackOrientation.Vertical
+            };
+
+            layout.Children.Add(new Label {  Text = "Label 1" });
+            layout.Children.Add(new Label { Text = "Label 1" });
+            layout.Children.Add(new Label { Text = "Label 1" });
+            layout.Children.Add(new Label { Text = "Label 1" });
+            layout.Children.Add(new Label { Text = "Label 1" });
+            layout.Children.Add(new Label { Text = "Label 1" });
+
+
+            // Muy importante, no olvidar la siguiente línea, que es
+            // en la que fijamos el contenido del layout
+            Content = layout;
         }
     }
 }
